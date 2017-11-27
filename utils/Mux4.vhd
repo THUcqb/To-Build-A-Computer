f656d8f22d9f3059bd1687bca410ae829fcb70a2
左浩佳 <zuohj15@mails.tsinghua.edu.cn>
2017-11-27 14:43:02 +0800

    feat: change mux types to 2, 4 and 8

--- utils/Mux4.vhd
+++ utils/Mux4.vhd
@@ -3,18 +3,19 @@ use IEEE.std_logic_1164.ALL;
 use IEEE.std_logic_ARITH.ALL;
 use IEEE.std_logic_UNSIGNED.ALL;
 
-entity Mux3 is
+entity Mux4 is
     port (
         i0: in std_logic_vector(15 downto 0);
         i1: in std_logic_vector(15 downto 0);
         i2: in std_logic_vector(15 downto 0);
+        i3: in std_logic_vector(15 downto 0);
         s: in std_logic_vector(1 downto 0);
 
         o: out std_logic_vector(15 downto 0)
     );
-end Mux3;
+end Mux4;
 
-architecture Mux3_beh of Mux3 is
+architecture Mux4_beh of Mux4 is
 
 begin
 
@@ -22,6 +23,7 @@ begin
         i0 when "00",
         i1 when "01",
         i2 when "10",
+        i3 when "11",
         i0 when others;
 
-end Mux3_beh;
+end Mux4_beh;
